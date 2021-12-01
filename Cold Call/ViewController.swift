7c2d75818264e3a81535adde50c0a8825e42309d
Safa Falaqi <sfalaqi@gmail.com>
2021-12-01 18:03:11 +0300

    ColdCall v.2

--- Cold Call/ViewController.swift
+++ Cold Call/ViewController.swift
@@ -12,15 +12,28 @@ class ViewController: UIViewController {
     var array = ["Tiffany","Mike","Charles","Justin"]
     @IBOutlet weak var name: UILabel!
     
+    @IBOutlet weak var number: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         name.text = array[Int.random(in: 0..<array.count)]
+        setNumber()
     }
 
 
     @IBAction func changeRandomly(_ sender: Any) {
         name.text = array[Int.random(in: 0..<array.count)]
+        setNumber()
+    }
+    func setNumber()
+    {
+        let rand = Int.random(in: 1...5)
+        switch(rand){
+        case 1 , 2 : number.textColor = UIColor.red
+        case 3 ,4 : number.textColor = UIColor.orange
+        default: number.textColor = UIColor.green
+        }
+        number.text = String(rand)
     }
 }
 
